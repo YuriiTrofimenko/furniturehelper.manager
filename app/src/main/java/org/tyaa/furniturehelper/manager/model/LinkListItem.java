12363fec416843306bdf0a386dd035ad01e760ff
YuriiTrofimenko <tyaa@ukr.net>
2017-11-29 16:27:33 +0200

    liks list item layout and model

---
+++ app/src/main/java/org/tyaa/furniturehelper/manager/model/LinkListItem.java
@@ -0,0 +1,17 @@
+package org.tyaa.furniturehelper.manager.model;
+
+import android.graphics.drawable.Drawable;
+
+import java.util.List;
+
+/**
+ * Created by yurii on 29.11.17.
+ */
+
+public class LinkListItem {
+
+    public String title;
+    public Drawable drawable;
+    public boolean checked;
+    public List<Link> links;
+}
