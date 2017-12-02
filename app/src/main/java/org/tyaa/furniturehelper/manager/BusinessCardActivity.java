61567090ede0bfbaacb7f83187a4b6e343bac765
YuriiTrofimenko <tyaa@ukr.net>
2017-12-02 19:19:43 +0200

    add links list

--- app/src/main/java/org/tyaa/furniturehelper/manager/BusinessCardActivity.java
+++ app/src/main/java/org/tyaa/furniturehelper/manager/BusinessCardActivity.java
@@ -1,9 +1,15 @@
 package org.tyaa.furniturehelper.manager;
 
+import android.app.Activity;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import org.tyaa.furniturehelper.manager.common.Global;
@@ -12,9 +18,48 @@ import org.tyaa.furniturehelper.manager.model.LinkList;
 import org.tyaa.furniturehelper.manager.model.LinkListItem;
 import org.tyaa.furniturehelper.manager.receiver.CallReceiver;
 
+import java.util.List;
+
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
+import butterknife.OnTouch;
+
 public class BusinessCardActivity extends AppCompatActivity {
 
+    @BindViews({R.id.viberTab, R.id.whatsappTab, R.id.telegramTab, R.id.smsTab})
+    List<View> mTabsList;
+
     private String mPhoneNumber = "";
+
+    /*@OnClick({R.id.viberTab, R.id.whatsappTab, R.id.telegramTab, R.id.smsTab})
+    void onClickTab(View linearLayout) {
+
+        ButterKnife.apply(
+                mTabsList
+                , (view, value, index) ->
+                        linearLayout.setBackgroundColor(value)
+                , android.R.color.transparent
+        );
+        Log.i("asd", linearLayout.getBackground().toString());
+        linearLayout.setBackgroundColor(Color.parseColor("#CCCCCC"));
+        switch (linearLayout.getId()) {
+            case R.id.viberTab:
+                // ...
+                break;
+            case R.id.whatsappTab:
+                // ...
+                break;
+            case R.id.telegramTab:
+                // ...
+                break;
+            case R.id.smsTab:
+                // ...
+                break;
+        }
+        //return true;
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +67,7 @@ public class BusinessCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_business_card);
+        ButterKnife.bind(this);
 
         mPhoneNumber =
                 getIntent().getStringExtra(CallReceiver.EXTRA_PHONE_NUMBER);
@@ -34,8 +80,45 @@ public class BusinessCardActivity extends AppCompatActivity {
         activityBusinessCardBinding.setItems(linkList);
         /*Toast.makeText(this, String.valueOf(c.size()),
                 Toast.LENGTH_SHORT).show();*/
-        for (LinkListItem linkListItem:linkList.mLinkItemList) {
+        /*for (LinkListItem linkListItem:linkList.mLinkItemList) {
             Log.i("asd", linkListItem.title);
-        }
+        }*/
+        /*for (View view : mTabsList) {
+
+            //Log.i("asd", "dsfdsgsg");
+            view.setOnClickListener(v -> {
+                Log.i("asd", "!!!");
+                ButterKnife.apply(
+                        mTabsList
+                        , (view1, value, index) ->
+                                view1.setBackgroundColor(value)
+                        , android.R.color.transparent
+                );
+                Log.i("asd", view.getBackground().toString());
+                view.setBackgroundColor(Color.parseColor("#CCCCCC"));
+            });
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("asd", "dsfdsgsg");
+                }
+            });
+        }*/
+
+        ImageView imageView = (ImageView) findViewById(R.id.smsTab);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i("asd", "dsfdsgsg");
+                ButterKnife.apply(
+                        mTabsList
+                        , (view, value, index) ->
+                                view.setBackgroundColor(value)
+                        , android.R.color.transparent
+                );
+                //Log.i("asd", view.getBackground().toString());
+                v.setBackgroundColor(Color.parseColor("#CCCCCC"));
+            }
+        });
     }
 }
--- app/src/main/java/org/tyaa/furniturehelper/manager/common/Generator.java
+++ app/src/main/java/org/tyaa/furniturehelper/manager/common/Generator.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.databinding.ObservableArrayList;
 
 import org.tyaa.furniturehelper.manager.R;
+import org.tyaa.furniturehelper.manager.model.Link;
 import org.tyaa.furniturehelper.manager.model.LinkListItem;
+
+import java.util.ArrayList;
 
 /**
  * Created by yurii on 30.11.17.
@@ -20,18 +23,69 @@ public class Generator {
         LinkListItem linkListItem =
                 new LinkListItem(
                         "ВКонтакте"
-                        , context.getResources().getDrawable(R.drawable.telegram)
+                        , context.getResources().getDrawable(R.drawable.vk)
                         , true
-                        , null
+                        , new ArrayList<Link>()
                 );
+        linkListItem.links.add(new Link());
+        linkListItem.links.add(new Link());
+        linkListItem.links.add(new Link());
         linkListItems.add(linkListItem);
         linkListItem =
                 new LinkListItem(
                         "Facebook"
-                        , context.getResources().getDrawable(R.drawable.viber)
+                        , context.getResources().getDrawable(R.drawable.facebook)
                         , true
-                        , null
+                        , new ArrayList<Link>()
                 );
+        linkListItem.links.add(new Link());
+        linkListItems.add(linkListItem);
+        linkListItem =
+                new LinkListItem(
+                        "Instagram"
+                        , context.getResources().getDrawable(R.drawable.instagram)
+                        , true
+                        , new ArrayList<Link>()
+                );
+        linkListItem.links.add(new Link());
+        linkListItems.add(linkListItem);
+        linkListItem =
+                new LinkListItem(
+                        "YouTube"
+                        , context.getResources().getDrawable(R.drawable.youtube)
+                        , true
+                        , new ArrayList<Link>()
+                );
+        linkListItem.links.add(new Link());
+        linkListItems.add(linkListItem);
+        linkListItem =
+                new LinkListItem(
+                        "Одноклассники"
+                        , context.getResources().getDrawable(R.drawable.odnoklassniki)
+                        , true
+                        , new ArrayList<Link>()
+                );
+        linkListItem.links.add(new Link());
+        linkListItems.add(linkListItem);
+        linkListItem =
+                new LinkListItem(
+                        "Site"
+                        , context.getResources().getDrawable(R.drawable.lidzalogo)
+                        , true
+                        , new ArrayList<Link>()
+                );
+        linkListItem.links.add(new Link());
+        linkListItems.add(linkListItem);
+        linkListItem =
+                new LinkListItem(
+                        "Google maps"
+                        , context.getResources().getDrawable(R.drawable.google_maps)
+                        , true
+                        , new ArrayList<Link>()
+                );
+        linkListItem.links.add(new Link());
+        linkListItem.links.add(new Link());
+        linkListItem.links.add(new Link());
         linkListItems.add(linkListItem);
         return linkListItems;
     }
