d5426619c933c898122d0533d9b2a0a4e57220c1
YuriiTrofimenko <tyaa@ukr.net>
2017-12-05 17:15:29 +0200

    mark tabs

--- app/src/main/java/org/tyaa/furniturehelper/manager/BusinessCardActivity.java
+++ app/src/main/java/org/tyaa/furniturehelper/manager/BusinessCardActivity.java
@@ -3,6 +3,7 @@ package org.tyaa.furniturehelper.manager;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,7 @@ import org.tyaa.furniturehelper.manager.model.LinkList;
 import org.tyaa.furniturehelper.manager.model.LinkListItem;
 import org.tyaa.furniturehelper.manager.receiver.CallReceiver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindViews;
@@ -28,37 +30,40 @@ import butterknife.OnTouch;
 
 public class BusinessCardActivity extends AppCompatActivity {
 
-    @BindViews({R.id.viberTab, R.id.whatsappTab, R.id.telegramTab, R.id.smsTab})
-    List<View> mTabsList;
+    //@BindViews({R.id.viberTabLayout, R.id.whatsappTabLayout, R.id.telegramTabLayout, R.id.smsTabLayout})
+    //List<View> mTabs;
+
+    private LinearLayout mViberTabLayout;
+    private LinearLayout mWhatsappTabLayout;
+    private LinearLayout mTelegramTabLayout;
+    private LinearLayout mSmsTabLayout;
+
+    private List<LinearLayout> mTabLayouts = new ArrayList<>();
+
+    private ImageView mViberTabImageView;
+    private ImageView mWhatsappTabImageView;
+    private ImageView mTelegramTabImageView;
+    private ImageView mSmsTabImageView;
+
+    private List<ImageView> mTabImageViews = new ArrayList<>();
 
     private String mPhoneNumber = "";
 
     /*@OnClick({R.id.viberTab, R.id.whatsappTab, R.id.telegramTab, R.id.smsTab})
-    void onClickTab(View linearLayout) {
+    void onClickTab(View selectedView) {
 
+        for (View item : mTabsList) {
+
+            Log.i("asd", item.toString());
+        }
         ButterKnife.apply(
                 mTabsList
                 , (view, value, index) ->
-                        linearLayout.setBackgroundColor(value)
+                        view.setBackgroundColor(value)
                 , android.R.color.transparent
         );
-        Log.i("asd", linearLayout.getBackground().toString());
-        linearLayout.setBackgroundColor(Color.parseColor("#CCCCCC"));
-        switch (linearLayout.getId()) {
-            case R.id.viberTab:
-                // ...
-                break;
-            case R.id.whatsappTab:
-                // ...
-                break;
-            case R.id.telegramTab:
-                // ...
-                break;
-            case R.id.smsTab:
-                // ...
-                break;
-        }
-        //return true;
+        Log.i("asd", selectedView.getBackground().toString());
+        selectedView.setBackgroundColor(Color.parseColor("#CCCCCC"));
     }*/
 
     @Override
@@ -67,7 +72,7 @@ public class BusinessCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_business_card);
-        ButterKnife.bind(this);
+        //ButterKnife.bind(this);
 
         mPhoneNumber =
                 getIntent().getStringExtra(CallReceiver.EXTRA_PHONE_NUMBER);
@@ -78,47 +83,81 @@ public class BusinessCardActivity extends AppCompatActivity {
                 DataBindingUtil.setContentView(this, R.layout.activity_business_card);
         final LinkList linkList = Global.LINK_LIST;
         activityBusinessCardBinding.setItems(linkList);
-        /*Toast.makeText(this, String.valueOf(c.size()),
-                Toast.LENGTH_SHORT).show();*/
-        /*for (LinkListItem linkListItem:linkList.mLinkItemList) {
-            Log.i("asd", linkListItem.title);
-        }*/
-        /*for (View view : mTabsList) {
 
-            //Log.i("asd", "dsfdsgsg");
-            view.setOnClickListener(v -> {
-                Log.i("asd", "!!!");
-                ButterKnife.apply(
-                        mTabsList
-                        , (view1, value, index) ->
-                                view1.setBackgroundColor(value)
-                        , android.R.color.transparent
-                );
-                Log.i("asd", view.getBackground().toString());
-                view.setBackgroundColor(Color.parseColor("#CCCCCC"));
-            });
-            view.setOnClickListener(new View.OnClickListener() {
+        mViberTabLayout = (LinearLayout) findViewById(R.id.viberTabLayout);
+        mWhatsappTabLayout = (LinearLayout) findViewById(R.id.whatsappTabLayout);
+        mTelegramTabLayout = (LinearLayout) findViewById(R.id.telegramTabLayout);
+        mSmsTabLayout = (LinearLayout) findViewById(R.id.smsTabLayout);
+
+        mTabLayouts.add(mViberTabLayout);
+        mTabLayouts.add(mWhatsappTabLayout);
+        mTabLayouts.add(mTelegramTabLayout);
+        mTabLayouts.add(mSmsTabLayout);
+
+        mViberTabImageView = (ImageView) findViewById(R.id.viberTabImageView);
+        mWhatsappTabImageView = (ImageView) findViewById(R.id.whatsappTabImageView);
+        mTelegramTabImageView = (ImageView) findViewById(R.id.telegramTabImageView);
+        mSmsTabImageView = (ImageView) findViewById(R.id.smsTabImageView);
+
+        mTabImageViews.add(mViberTabImageView);
+        mTabImageViews.add(mWhatsappTabImageView);
+        mTabImageViews.add(mTelegramTabImageView);
+        mTabImageViews.add(mSmsTabImageView);
+
+        int tabIdx = 0;
+        for (ImageView imageView : mTabImageViews) {
+
+            final int currentTabIdx = tabIdx;
+            imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Log.i("asd", "dsfdsgsg");
+
+                    for (LinearLayout linearLayout : mTabLayouts) {
+
+                        linearLayout.setBackgroundColor(getResources()
+                            .getColor(android.R.color.transparent));
+                    }
+                    mTabLayouts.get(currentTabIdx)
+                        .setBackgroundColor(Color.parseColor("#CCCCCC"));
                 }
             });
-        }*/
+            tabIdx++;
+        }
 
-        ImageView imageView = (ImageView) findViewById(R.id.smsTab);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        /*ImageView imageView1 = (ImageView) findViewById(R.id.viberTab);
+        imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Log.i("asd", "dsfdsgsg");
+                Log.i("asd", "1");
                 ButterKnife.apply(
-                        mTabsList
+                        mTabs
                         , (view, value, index) ->
                                 view.setBackgroundColor(value)
                         , android.R.color.transparent
                 );
                 //Log.i("asd", view.getBackground().toString());
-                v.setBackgroundColor(Color.parseColor("#CCCCCC"));
+                //mTabs.get(0).setBackgroundColor(Color.parseColor("#CCCCCC"));
+                ((LinearLayout) findViewById(R.id.viberTabLayout))
+                        .setBackgroundColor(Color.parseColor("#CCCCCC"));
             }
-        });
+        });*/
+
+        /*ImageView imageView2 = (ImageView) findViewById(R.id.smsTab);
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("asd", "4");
+                ButterKnife.apply(
+                        mTabs
+                        , (view, value, index) ->
+                                view.setBackgroundColor(value)
+                        , android.R.color.transparent
+                );
+                //Log.i("asd", view.getBackground().toString());
+                //mTabs.get(3).setBackgroundColor(Color.parseColor("#CCCCCC"));
+                ((LinearLayout) findViewById(R.id.smsTabLayout))
+                        .setBackgroundColor(Color.parseColor("#CCCCCC"));
+            }
+        });*/
     }
 }
